0af131889f37404acfea42368398ccd06bfb1a50
Enes Ekinci <enes.eknc.96@gmail.com>
2023-08-16 21:39:11 +0300

    response message codes defined

---
+++ app/helpers/error_messages.js
@@ -0,0 +1,17 @@
+
+const EMAIL = 1;
+const PASSWORD = 2;
+const REQUIRED = 3;
+const PATTERN = 4;
+const STRING = 5;
+
+module.exports = {
+    EMAIL,
+    REQUIRED,
+    PATTERN,
+    STRING,
+    PASSWORD,
+};
+
+
+
